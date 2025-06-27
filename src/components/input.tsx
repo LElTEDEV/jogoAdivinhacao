c5c8f03f30a7e43c7a879eee59486ded845c9106
LElTEDEV <zinenizi@gmail.com>
2025-06-27 16:59:13 -0300

    created input component

---
+++ src/components/input.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps } from "react";
+
+interface Props extends ComponentProps<"input"> {}
+
+export function Input({ ...rest }: Props) {
+  return (
+    <input
+      className="w-[2.87rem] h-[2.87rem] outline-0 rounded-md bg-violet-100 border-2 border-violet-400 uppercase text-center font-bold font-sans text-violet-900"
+      type="text"
+      {...rest}
+    />
+  );
+}
