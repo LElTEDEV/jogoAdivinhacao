f1fb5af816ace85dfd5b209c98fb95d8eba7b63a
LElTEDEV <zinenizi@gmail.com>
2025-06-27 15:08:02 -0300

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <main className="w-[442px]">
-        <Header />
+        <Header current={7} max={10} onRestart={() => console.log("UE")} />
       </main>
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,24 @@
 import logoSvg from "../assets/logo.svg";
 import refreshSvg from "../assets/refresh.svg";
 
-export function Header() {
+interface Props {
+  current: number;
+  max: number;
+  onRestart: () => void;
+}
+
+export function Header({ current = 0, max = 0, onRestart }: Props) {
   return (
     <div className="w-full flex flex-col items-center">
       <img className="h-[3.75rem]" src={logoSvg} alt="Adivinhação" />
 
       <header className="w-full flex justify-between py-[3.25rem] px-0">
         <span className="text-[1.12rem] text-gray-400">
-          <strong className="text-[1.37rem] text-orange-400">5</strong> de 10
-          tentativas
+          <strong className="text-[1.37rem] text-orange-400">{current}</strong>{" "}
+          de {max} tentativas
         </span>
 
-        <button className="" type="button">
+        <button onClick={onRestart} type="button">
           <img
             className="h-8 transition ease-in cursor-pointer hover:scale-105 hover:animate-spin"
             src={refreshSvg}
